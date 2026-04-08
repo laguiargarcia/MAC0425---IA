76f698c82ce135bdf006660648f5cd459c900afb
laguiargarcia <laguiargarcia@outlook.com>
2026-04-08 18:23:00 -0300

    update

--- EP1/search/search.py
+++ EP1/search/search.py
@@ -11,7 +11,7 @@
 # Student side autograding was added by Brad Miller, Nick Hay, and
 # Pieter Abbeel (pabbeel@cs.berkeley.edu).
 
-
+# LUCAS AGUIAR GARCIA - 13672770
 """
 In search.py, you will implement generic search algorithms which are called by
 Pacman agents (in searchAgents.py).
